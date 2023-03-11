da1a77c9ac2131bc8063d9190ec949f16a6e3e8a
Floris Bruynooghe <flub@devork.be>
2023-03-11 20:29:50 +0100

    feat: basic oneshot netlink

--- src/interface/linux.rs
+++ src/interface/linux.rs
@@ -1,6 +1,45 @@
+use bytes::BytesMut;
+use netlink_packet_core::{NetlinkMessage, NetlinkPayload, NLM_F_DUMP, NLM_F_REQUEST};
+use netlink_packet_route::{AddressMessage, RtnlMessage};
+use netlink_sys::constants::NETLINK_ROUTE;
+use netlink_sys::{Socket, SocketAddr};
+
 use crate::interface::InterfaceType;
 use std::convert::TryFrom;
 use std::fs::read_to_string;
+
+fn interfaces_netlink() {
+    let socket = Socket::new(NETLINK_ROUTE).unwrap();
+    let mut req = NetlinkMessage::from(RtnlMessage::GetAddress(AddressMessage::default()));
+    req.header.sequence_number = 1;
+    req.header.flags = NLM_F_REQUEST | NLM_F_DUMP;
+    req.finalize();
+    let mut buf = vec![0; req.header.length as usize];
+    req.serialize(&mut buf);
+    let addr = SocketAddr::new(0, 0);
+    let n = socket.send_to(&buf, &addr, 0).unwrap();
+    assert_eq!(n, req.header.length as usize);
+    let mut buf = BytesMut::with_capacity(4096);
+    loop {
+        let (n, addr) = socket.recv_from(&mut buf, 0).unwrap();
+        let packet = NetlinkMessage::<RtnlMessage>::deserialize(&buf).unwrap();
+        println!("{packet:?}");
+        buf.clear();
+        if matches!(packet.payload, NetlinkPayload::Done) {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interfaces_netlink() {
+        interfaces_netlink();
+    }
+}
 
 pub fn get_interface_type(if_name: String) -> InterfaceType {
     let if_type_path: String = format!("/sys/class/net/{}/type", if_name);
